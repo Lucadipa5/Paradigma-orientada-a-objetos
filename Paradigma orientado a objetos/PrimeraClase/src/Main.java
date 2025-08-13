6294d23e3ca2e866b463f1c97677ef95da62f69f
Luca_dp:5 <lucadipasquale35@gmail.com>
2025-08-13 20:13:44 -0300

    Clase 2

--- Paradigma orientado a objetos/PrimeraClase/src/Main.java
+++ Paradigma orientado a objetos/PrimeraClase/src/Main.java
@@ -1,29 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+    ascensor ascensor = new ascensor();
+    //            Pistola pistola = new Pistola();
+//            System.out.println("=== Estado inicial ===");
+//                System.out.println("¿Está recargada?: " + pistola.estaRecargada());
+//                System.out.println("Cantidad de balas: " + pistola.cuantasBalasTengo());
+//                System.out.println("¿Está limpia?: " + pistola.estaLimpia());
+//
+//                System.out.println("\n--- Recargando pistola ---");
+//                pistola.recargar();
+//                System.out.println("¿Está recargada?: " + pistola.estaRecargada());
+//                System.out.println("Cantidad de balas: " + pistola.cuantasBalasTengo());
+//
+//                System.out.println("\n--- Limpiando pistola ---");
+//                pistola.limpiar();
+//                System.out.println("¿Está limpia?: " + pistola.estaLimpia());
+//            }
+        Persona alfrodoti = new Persona();// Objeto instacia = new objeto();
+        System.out.println("Alfrodoti esta en el piso: " + alfrodoti.pisoActual +
+                            " Y quiere ir al piso " + alfrodoti.pisoQueQuieroIr);
+        ascensor.ingresa(alfrodoti);
+        ascensor.comenzarRecorrido(alfrodoti);
+        ascensor.finalizarRecorrido(alfrodoti);
+        System.out.println(alfrodoti.pisoActual);
 
-        ascensor ascensorPrimeroDeIndependencia = new ascensor();
-        System.out.println(ascensorPrimeroDeIndependencia.isPuertaAbierta());
-        System.out.println(ascensorPrimeroDeIndependencia.enQuePisoEstas());
-        ascensorPrimeroDeIndependencia.cambiarPisos(10);
-        System.out.println(ascensorPrimeroDeIndependencia.enQuePisoEstas());
-
-            Pistola pistola = new Pistola();
-            System.out.println("=== Estado inicial ===");
-                System.out.println("¿Está recargada?: " + pistola.estaRecargada());
-                System.out.println("Cantidad de balas: " + pistola.cuantasBalasTengo());
-                System.out.println("¿Está limpia?: " + pistola.estaLimpia());
-
-                System.out.println("\n--- Recargando pistola ---");
-                pistola.recargar();
-                System.out.println("¿Está recargada?: " + pistola.estaRecargada());
-                System.out.println("Cantidad de balas: " + pistola.cuantasBalasTengo());
-
-                System.out.println("\n--- Limpiando pistola ---");
-                pistola.limpiar();
-                System.out.println("¿Está limpia?: " + pistola.estaLimpia());
-            }
-        }
+    }}
 
 
---
+++ Paradigma orientado a objetos/PrimeraClase/src/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    public int pisoActual = 8;
+    public int pisoQueQuieroIr = 24;
+    public boolean alfroInAscensor = false;
+
+    public int pisoAlfrodoti(){
+        return pisoActual;
+    }
+}
