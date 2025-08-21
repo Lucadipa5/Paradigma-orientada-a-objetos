dbfc880bde25ff7921f833fc6dbdc5f4237ccd96
Luca_dp:5 <lucadipasquale35@gmail.com>
2025-08-20 21:46:46 -0300

    Clase 3

--- Paradigma orientado a objetos/PrimeraClase/src/Persona.java
+++ Paradigma orientado a objetos/PrimeraClase/src/Persona.java
@@ -2,8 +2,17 @@ public class Persona {
     public int pisoActual = 8;
     public int pisoQueQuieroIr = 24;
     public boolean alfroInAscensor = false;
+    public Pulsera pulsera;
 
     public int pisoAlfrodoti(){
         return pisoActual;
+    }
+    public void asignarPulsera(Pulsera pulsera){
+       this.pulsera = pulsera;
+    }
+    public void intercambiarPulseras(Persona a){
+         Pulsera aux = a.pulsera;
+        a.pulsera = this.pulsera;
+        this.pulsera = aux;
     }
 }
---
+++ Paradigma orientado a objetos/PrimeraClase/src/Pulsera.java
@@ -0,0 +1,11 @@
+public class Pulsera {
+    private int precio;
+
+    public int cuantoPaga(){
+        return precio;
+    }
+    public void asignarPrecio(int n){
+        this.precio=n;
+    }
+
+}
